f3602cc08e158afd596d4bc635c3dbf5eb47e920
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-05 21:38:36 +0530

    Create Segregate 0s and 1s.java

---
+++ Day-07-Array-Rearrangement/Segregate 0s and 1s.java
@@ -0,0 +1,16 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int j = 0;
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
