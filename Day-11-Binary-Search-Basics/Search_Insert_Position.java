0c95d4b5e5c860f41e4a78214f519cd03b66813c
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-09 18:15:42 +0530

    Implement binary search for insert position

--- Day-11-Binary-Search-Basics/Search_Insert_Position.java
+++ Day-11-Binary-Search-Basics/Search_Insert_Position.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
 
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (nums[low] >= target)
+            return low;
+        return low + 1;
+    }
+}
