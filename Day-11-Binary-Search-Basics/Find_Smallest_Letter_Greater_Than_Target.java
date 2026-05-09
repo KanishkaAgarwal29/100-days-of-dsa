865e66f5ffb168c1e496d8e4e9b5c659c66a3c10
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-09 18:37:42 +0530

    Implement binary search for next greatest letter

--- Day-11-Binary-Search-Basics/Find_Smallest_Letter_Greater_Than_Target.java
+++ Day-11-Binary-Search-Basics/Find_Smallest_Letter_Greater_Than_Target.java
@@ -1,1 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int low = 0;
+        int high = n - 1;
+        char c = letters[0];
 
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (letters[high] > target)
+            return letters[high];
+        return letters[0];
+    }
+}
