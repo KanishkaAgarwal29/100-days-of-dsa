ec143b9fd2fc83b1dba83ebe275bc0428142dbf7
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-10 14:54:59 +0530

    Implement binary search for arranging coins

--- Day-12-Binary-Search-On-Answers/Arranging_Coins.java
+++ Day-12-Binary-Search-On-Answers/Arranging_Coins.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if (n <= 1) return n;
+        long low = 1;
+        long high = n;
+        long count = 0;
 
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            count = mid * (mid + 1) / 2;
+            if (count == n)
+                return (int) mid;
+            else if (count < n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) high;
+    }
+}
