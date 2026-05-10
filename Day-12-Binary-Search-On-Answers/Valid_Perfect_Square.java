7cbe915e6174e9434893766a810823a847b1dad4
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-10 14:11:57 +0530

    Add method to check if a number is a perfect square
    Implement binary search to check for perfect squares.

--- Day-12-Binary-Search-On-Answers/Valid_Perfect_Square.java
+++ Day-12-Binary-Search-On-Answers/Valid_Perfect_Square.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    static int checkPerfectSquare(int N) {
+        // code here
+        if(N <= 1)return 1;
+        int low  = 1;
+        int high = N/2;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if( mid * mid == N)return 1;
+            else if(mid > N / mid) high = mid - 1;
+            else low = mid + 1;
+        }
+        
+        return 0;
+    }
+}
