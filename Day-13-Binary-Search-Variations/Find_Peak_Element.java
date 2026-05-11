774cbfe25c8e303fd57de853e4a1083754b66ad6
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-11 11:10:27 +0530

    Implement binary search to find peak element

--- Day-13-Binary-Search-Variations/Find_Peak_Element.java
+++ Day-13-Binary-Search-Variations/Find_Peak_Element.java
@@ -1,1 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        if (n == 1)
+            return 0;
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
 
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if ((mid > 0 && nums[mid] > nums[mid - 1]) && (mid < nums.length - 1 && nums[mid] > nums[mid + 1]))
+                return mid;
+            else if (mid < nums.length - 1 && nums[mid] > nums[mid + 1]) {
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
