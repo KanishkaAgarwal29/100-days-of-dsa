79d4268baf80702425ef7659ab3725732e4f6504
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-11 21:06:57 +0530

    Update Single_Element_Sorted_Array.java

--- Day-13-Binary-Search-Variations/Single_Element_Sorted_Array.java
+++ Day-13-Binary-Search-Variations/Single_Element_Sorted_Array.java
@@ -1,1 +1,21 @@
-.
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
+                    (mid == nums.length - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            } else if ((mid % 2 != 0 && nums[mid] == nums[mid - 1])
+                    || (mid % 2 == 0 && nums[mid] == nums[mid + 1] && mid + 1 < nums.length)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+}.
