e14ef8318096ec5f0dc928448210817a6b2b6207
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-11 11:04:30 +0530

    Update Count_Occurrences.java

--- Day-13-Binary-Search-Variations/Count_Occurrences.java
+++ Day-13-Binary-Search-Variations/Count_Occurrences.java
@@ -1,1 +1,36 @@
+class Solution {
+    int countFreq(int[] nums, int target) {
+        // code here
+        int count = 0;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                count = 1;
+                int ind = mid;
+                while(ind - 1 >= 0 && nums[ind - 1] == nums[ind]){
+                    ind--;
+                    count++;
+                }
+                
+                ind = mid;
+                while(ind + 1 < nums.length && nums[ind + 1] == nums[ind]){
+                    ind++;
+                    count++;
+                }
+                
+                break;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return count;
+    }
+}
 
