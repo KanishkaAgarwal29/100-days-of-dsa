ed184bf50d3af827be89d5a560e9acc2e06026a0
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-11 11:01:07 +0530

    Update First_Last_Position.java

--- Day-13-Binary-Search-Variations/First_Last_Position.java
+++ Day-13-Binary-Search-Variations/First_Last_Position.java
@@ -1,1 +1,34 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                int ind = mid;
+                while(ind - 1 >= 0 && nums[ind - 1] == nums[ind]){
+                    ind--;
+                }
+                ans[0] = ind;
 
+                ind = mid;
+                while(ind + 1 < nums.length && nums[ind + 1] == nums[ind]){
+                    ind++;
+                }
+                ans[1] = ind;
+                break;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
