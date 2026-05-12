9d8a339fe66b76465f9a731b7fa38bf4a61a485a
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-12 13:21:58 +0530

    Update 1_Search_Rotated_Array.java

--- Day-14-Rotated-Sorted-Arrays/1_Search_Rotated_Array.java
+++ Day-14-Rotated-Sorted-Arrays/1_Search_Rotated_Array.java
@@ -1,1 +1,52 @@
+class Solution {
+    public int minimum(int[] nums,int target){
+        int low=0;
+        int high=nums.length-1;
 
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+            
+        }
+        return high;
+    }
+
+    public int bs(int[] nums, int target,int low,int high){
+        
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+                
+            }
+            else{
+                low=mid+1;
+            }
+            
+    }
+    return -1;
+}
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int minidx=minimum(nums,target);
+
+        int left=bs(nums,target,0,minidx);
+        if(left==-1){
+            int right=bs(nums,target,minidx,n-1);
+            return right;
+        }
+        return left;
+    }
+}
