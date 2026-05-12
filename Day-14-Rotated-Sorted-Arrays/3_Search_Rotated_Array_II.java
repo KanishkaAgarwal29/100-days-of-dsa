ebb48ece79daebf6cdff238a654fb265a2fc377b
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-12 20:29:25 +0530

    Implement search in rotated sorted array

--- Day-14-Rotated-Sorted-Arrays/3_Search_Rotated_Array_II.java
+++ Day-14-Rotated-Sorted-Arrays/3_Search_Rotated_Array_II.java
@@ -1,1 +1,34 @@
+class Solution {
+    public boolean search(int[] nums, int key) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == key) return true;
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (key >= nums[low] && key < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (key > nums[mid] && key <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
 
