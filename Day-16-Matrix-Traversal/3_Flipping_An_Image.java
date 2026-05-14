cb101fa487ef623d2bbbc28e5af315e82b5b5b88
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-14 22:08:40 +0530

    Fix image flipping and inverting logic

--- Day-16-Matrix-Traversal/3_Flipping_An_Image.java
+++ Day-16-Matrix-Traversal/3_Flipping_An_Image.java
@@ -2,11 +2,9 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int m = image.length;
         int n = image.length;
-
         for (int i = 0; i < m; i++) {
             int start = 0;
             int end = n - 1;
-
             while (start <= end) {
                 if (image[i][start] == image[i][end]) {
                     image[i][start] ^= 1;
