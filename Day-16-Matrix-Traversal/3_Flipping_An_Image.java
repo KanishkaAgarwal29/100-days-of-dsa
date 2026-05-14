074886c8fa4d2074b4a37e4f236a4bae6e391e11
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-14 22:02:23 +0530

    Update and rename 3_Diagonal_Traverse.java to 3_Flipping_An_Image.java

---
+++ Day-16-Matrix-Traversal/3_Flipping_An_Image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int m = image.length;
+        int n = image.length;
+
+        for (int i = 0; i < m; i++) {
+            int start = 0;
+            int end = n - 1;
+
+            while (start <= end) {
+                if (image[i][start] == image[i][end]) {
+                    image[i][start] ^= 1;
+                    if (start < end) {
+                        image[i][end] ^= 1;
+                    }
+                }
+                start++;
+                end--;
+            }
+        }
+        return image;
+    }
+}
