0f6bae9e4d288196021c529c345c3eef463b59b0
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-15 22:23:39 +0530

    Implement grid shifting functionality

--- Day-17-Matrix-Traversal/4_Shift_2D_Grid.java
+++ Day-17-Matrix-Traversal/4_Shift_2D_Grid.java
@@ -1,1 +1,28 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
 
+        k %= m * n;
+        int[][] newGrid = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = (i * n + j + k) % total;
+                newGrid[idx / n][idx % n] = grid[i][j];
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            ans.add(new ArrayList<>());
+
+            for (int j = 0; j < n; j++) {
+                ans.get(i).add(newGrid[i][j]);
+            }
+        }
+
+        return ans;
+    }
+}
