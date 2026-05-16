5d9313dc66564a3924b93b60c3f40ba808240911
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-16 16:10:43 +0530

    Create 3_Row_With_Max_1s.java

---
+++ Day-18-Matrix-Searching/3_Row_With_Max_1s.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rowWithMax1s(int[][] arr) {
+        // code here
+        int m = arr.length;
+        int n = arr[0].length;
+        
+        int i = 0;
+        int j = n - 1;
+        
+        int maxRow = -1;
+        
+        while(i < m && j >= 0){
+            if(arr[i][j] == 0)i++;
+            else{
+                j--;
+                maxRow = i;
+            }
+        }
+        return maxRow;
+    }
+}
