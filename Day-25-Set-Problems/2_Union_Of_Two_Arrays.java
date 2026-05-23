0038efd0daea2602d81ccd6af3987b380e1e11db
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-05-23 14:10:21 +0530

    Implement union of two arrays using HashSet

---
+++ Day-25-Set-Problems/2_Union_Of_Two_Arrays.java
@@ -0,0 +1,11 @@
+class Solution {
+	public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+		// code here
+		HashSet<Integer> set = new HashSet<>();
+		
+		for (int i : a)set.add(i);
+		for (int j : b)set.add(j);
+		
+		return new ArrayList<>(set);
+	}
+}
