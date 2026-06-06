51ce1e93db68b8ebf7aaf788659c95b794b9e610
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-06-06 22:18:48 +0530

    Implement containsNearbyDuplicate method

---
+++ Day-39-Fixed-Window-And-Sorted-Window/1_Contains_Duplicate_II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (mpp.containsKey(nums[i]) && i - mpp.get(nums[i]) <= k) {
+                return true;
+            } else {
+                mpp.put(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+}
