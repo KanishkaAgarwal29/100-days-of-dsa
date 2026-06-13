1e59b171789a0de1d133afc82687d058aaecb06f
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-06-13 22:14:45 +0530

    Implement stack sequence validation method

---
+++ Day-46-Stack-Applications/3_Validate_Stack_Sequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int j = 0;
+        Stack<Integer> st = new Stack<>();
+
+        for(int num : pushed) {
+            st.push(num);
+            while (!st.empty() && st.peek() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+ 
+        return st.isEmpty();
+    }
+}
