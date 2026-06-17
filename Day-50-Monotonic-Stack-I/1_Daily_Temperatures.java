be04290fe5ed08b05274e7a1b9911d48eb809f90
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-06-17 17:13:50 +0530

    Implement dailyTemperatures function using monotonic stack

---
+++ Day-50-Monotonic-Stack-I/1_Daily_Temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Deque<Integer> st = new ArrayDeque<>();
+        int n = temp.length;
+        int[] ans = new int[n];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && temp[i] >= temp[st.peek()]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) {
+                ans[i] = st.peek() - i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
