4651deebe8d041cb884719aef06358f87099ae85
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-06-18 12:48:44 +0530

    Implement maxWidthRamp function using monotonic stack

---
+++ Day-51-Monotonic-Stack-II/4_Maximum_Width_Ramp.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        Deque<Integer> st = new ArrayDeque<>();
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (st.isEmpty() || nums[i] < nums[st.peek()]) {
+                st.push(i);
+            }
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                max = Math.max(max, i - st.peek());
+                st.pop();
+            }
+        }
+        return max;
+    }
+}
