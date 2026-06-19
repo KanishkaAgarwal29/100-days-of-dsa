7848103681256b65f5371a7154da072383e4da8a
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-06-19 13:19:38 +0530

    Implement smallest subsequence of distinct characters

---
+++ Day-52-Monotonic-Stack-Applications/3_Smallest_Subsequence_Of_Distinct_Characters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] freq = new int[26];
+        boolean[] check = new boolean[26];
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']--;
+            if (check[c - 'a'])
+                continue;
+
+            while (sb.length() > 0 && sb.charAt(sb.length() - 1) >= c && freq[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                check[sb.charAt(sb.length() - 1) - 'a'] = false;
+                sb.deleteCharAt(sb.length() - 1);
+            }
+            sb.append(c);
+            check[c - 'a'] = true;
+        }
+
+        return sb.toString();
+    }
+}
