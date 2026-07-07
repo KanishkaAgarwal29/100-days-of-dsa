f6b8c8f00d4e7973537dfa27ac67b6365f695968
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-07-07 22:40:16 +0530

    Implement longestPalindrome method in Solution class

---
+++ Day-60-Greedy-VI/3_Longest_Palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch]++;
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for (int count : freq) {
+            length += (count / 2) * 2;
+
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+
+        return hasOdd ? length + 1 : length;
+    }
+}
