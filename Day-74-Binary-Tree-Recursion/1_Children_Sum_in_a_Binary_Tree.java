3c752622dd20ce5c595a348c807e5c589b5285ce
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-07-21 21:11:20 +0530

    Implement isSumProperty method for binary tree

---
+++ Day-74-Binary-Tree-Recursion/1_Children_Sum_in_a_Binary_Tree.java
@@ -0,0 +1,21 @@
+/* Node Structure
+class Node{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+} */
+class Solution {
+    public boolean isSumProperty(Node root) {
+        //  code here
+        if(root == null || (root.left == null && root.right == null))return true;
+        int sum = 0;
+        if(root.left != null)sum+=root.left.data;
+        if(root.right != null)sum+=root.right.data;
+        if(sum != root.data)return false;
+        return isSumProperty(root.left) && isSumProperty(root.right);
+    }
+}
