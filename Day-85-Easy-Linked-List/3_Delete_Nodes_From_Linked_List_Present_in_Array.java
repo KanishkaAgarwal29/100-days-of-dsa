f4c3da922e23be05baa4093e3a1d729695199b0e
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-08-01 20:41:48 +0530

    Add method to delete nodes from linked list
    Implement a solution to delete nodes from a linked list that are present in a given array.

---
+++ Day-85-Easy-Linked-List/3_Delete_Nodes_From_Linked_List_Present_in_Array.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        ListNode newHead = new ListNode(-1);
+        ListNode prev = newHead;
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (!set.contains(temp.val)) {
+                prev.next = temp;
+                prev = prev.next;
+            }
+
+            temp = temp.next;
+        }
+
+        prev.next = null;
+        return newHead.next;
+
+    }
+}
