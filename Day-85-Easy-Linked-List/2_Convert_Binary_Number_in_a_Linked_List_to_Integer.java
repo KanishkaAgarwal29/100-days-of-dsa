7c34283fc754ac294bcd5b42692a772ec16b344c
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-08-01 20:41:21 +0530

    Add method to convert binary linked list to integer
    Implement a method to convert a binary number represented by a linked list to its decimal value.

---
+++ Day-85-Easy-Linked-List/2_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public int getDecimalValue(ListNode head) {
+
+        ListNode temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+
+        temp=head;
+        int num=0;
+        while(temp!=null){
+            count--;
+            num+=Math.pow(2,count)*temp.val;
+            temp=temp.next;
+        }
+        return num;
+    }
+}
