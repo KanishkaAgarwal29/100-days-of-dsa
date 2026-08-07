453ef9a6bb557736856bc4b3fbf934c9e7a855f4
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2026-08-07 20:12:36 +0530

    Implement DFS to count number of enclaves

---
+++ Day-91-Graphs-Boundary-DFS/2_Number_of_Enclaves.java
@@ -0,0 +1,52 @@
+class Solution {
+    int[] drow = { 1, 0, -1, 0 };
+    int[] dcol = { 0, 1, 0, -1 };
+
+    public void dfs(int[][] grid, int row, int col) {
+        grid[row][col] = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < 4; i++) {
+            int r = drow[i] + row;
+            int c = dcol[i] + col;
+
+            if ((r >= 0 && r < m) && (c >= 0 && c < n) && grid[r][c] == 1) {
+                dfs(grid, r, c);
+            }
+        }
+    }
+
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0);
+            }
+            if (grid[i][n - 1] == 1) {
+                dfs(grid, i, n - 1);
+            }
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            if (grid[0][i] == 1) {
+                dfs(grid, 0, i);
+            }
+            if (grid[m - 1][i] == 1) {
+                dfs(grid, m - 1, i);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
